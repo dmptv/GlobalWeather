809d5504e944e9e3abbe67dbb2bbdc45ff37d0a4
kanat <kabsadyk@eub.kz>
2023-08-05 20:17:08 +0600

    implement future for remote calls

--- GlobalWeather/Common/API/APIClientService/APIClientService.swift
+++ GlobalWeather/Common/API/APIClientService/APIClientService.swift
@@ -12,22 +12,29 @@ import Combine
 // https://medium.com/@AladinWay/write-a-networking-layer-in-swift-4-using-alamofire-5-and-codable-part-3-using-futures-promises-cf3977fc8a5
 
 protocol APIClientServiceProtocol {
-    func cityWeather(cityName: String, completion: @escaping (Result<WeatherResponse, AFError>) -> Void)
+    func cityWeather(cityName: String) -> Future<WeatherResponse, AFError>
 }
 
 class APIClientService: APIClientServiceProtocol {
-    func cityWeather(cityName: String, completion: @escaping (Result<WeatherResponse, AFError>) -> Void) {
-        performRequest(route: APIRouter.city(name: cityName), completion: completion)
+    func cityWeather(cityName: String) -> Future<WeatherResponse, AFError> {
+        performRequest(route: .city(name: cityName))
     }
 }
     
 extension APIClientService {
     @discardableResult
-    private func performRequest<T: Decodable>(route: APIRouter, decoder: JSONDecoder = JSONDecoder(), completion:@escaping (Result<T, AFError>) -> Void) -> DataRequest {
-        return AF.request(route)
-            .responseDecodable (decoder: decoder) { (response: DataResponse<T, AFError>) in
-                completion(response.result)
-            }
+    private func performRequest<T: Decodable>(route: APIRouter, decoder: JSONDecoder = JSONDecoder()) -> Future<T, AFError> {
+        return Future { promise in
+            AF.request(route)
+                .responseDecodable (decoder: decoder) { (response: DataResponse<T, AFError>) in
+                    switch response.result {
+                    case .success(let value):
+                        promise(.success(value))
+                    case .failure(let error):
+                        promise(.failure(error))
+                    }
+                }
+        }
     }
 }
 
--- GlobalWeather/Sources/Flows/FirstFlow/FlowCoordinator/Modules/MainModule/Interactor/MainInteractor.swift
+++ GlobalWeather/Sources/Flows/FirstFlow/FlowCoordinator/Modules/MainModule/Interactor/MainInteractor.swift
@@ -7,10 +7,12 @@
 //
 
 import Alamofire
+import Combine
 
 class MainInteractor {
+    private var cancellables = Set<AnyCancellable>()
+
     typealias ServiceLocatorAlias = APIClientServiceLocator
-    
     final class ServiceLocator: ServiceLocatorAlias {}
     
     weak var output: MainInteractorOutput?
@@ -19,16 +21,24 @@ class MainInteractor {
     init(serviceLocator: ServiceLocatorAlias = ServiceLocator()) {
         apiClient = serviceLocator.serviceAPIClient()
         
-        apiClient.cityWeather(cityName: "Paris") { result in
-            switch result {
-            case let .success(weather):
-                print(weather)
-            case let .failure(error):
-                print(error)
-            }
-        }
+        apiClient.cityWeather(cityName: "Paris")
+            .sink(receiveCompletion: { receiveCompletion in
+                switch receiveCompletion {
+                case .finished:
+                    print("finished")
+                case .failure(let error):
+                    print(error, "kanat error")
+                }
+            }, receiveValue: { response in
+                print(response, "response kanat")
+            })
+            .store(in: &cancellables)
     }
 
+    deinit {
+        cancellables.forEach { $0.cancel() }
+        cancellables.removeAll()
+    }
 }
 
 // MARK: Private
