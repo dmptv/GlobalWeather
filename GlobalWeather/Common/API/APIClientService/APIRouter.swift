cfd65b288f34428b972bcc3d6e3725c50d950743
kanat <kabsadyk@eub.kz>
2023-08-06 14:40:30 +0600

    query parameters

--- GlobalWeather/Common/API/APIClientService/APIRouter.swift
+++ GlobalWeather/Common/API/APIClientService/APIRouter.swift
@@ -7,6 +7,11 @@
 
 import Alamofire
 
+// https://api.openweathermap.org/data/2.5/forecast?
+// q=Paris&
+// APPID=da69ade359c47e35161bf2e2dad374e8&
+// units=metric
+
 enum APIRouter: URLRequestConvertible {
     private static let baseURL = "https://api.openweathermap.org/data/2.5/forecast"
     private static let API_KEY = "da69ade359c47e35161bf2e2dad374e8"
@@ -25,39 +30,51 @@ enum APIRouter: URLRequestConvertible {
     // MARK: - Path
     private var path: String {
         switch self {
-        case let .city(cityName):
-            return "/q=\(cityName)&APPID=\(APIRouter.API_KEY)&units=metric"
-        case let .wheatherBy(location):
-            return "/APPID=\(APIRouter.API_KEY)&lat=\(location.latitude)&lon=\(location.longitude)&units=metrics"
+        case .city:
+            return ""
+        case .wheatherBy:
+            return ""
         }
     }
     
     // MARK: - Parameters
     private var parameters: Parameters? {
         switch self {
-        default:
-            return nil
+        case let .city(cityName):
+            return [
+                "q": cityName,
+                "APPID": APIRouter.API_KEY,
+                "units": "metric"
+            ]
+        case let .wheatherBy(location):
+            return [
+                "APPID": APIRouter.API_KEY,
+                "lat": location.latitude,
+                "lon": location.longitude,
+                "units": "metric"
+            ]
         }
     }
     
-    // MARK: - URLRequestConvertible
     func asURLRequest() throws -> URLRequest {
         let url = try APIRouter.baseURL.asURL()
+        var urlComponents = URLComponents(url: url.appendingPathComponent(path), resolvingAgainstBaseURL: true)
         
-        var urlRequest = URLRequest(url: url.appendingPathComponent(path))
-        
-        urlRequest.httpMethod = method.rawValue
-        
-        urlRequest.setValue("application/json", forHTTPHeaderField: "Authorization")
-        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
- 
+        // Add query items to URL if parameters exist
         if let parameters = parameters {
-            do {
-                urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
-            } catch {
-                throw AFError.parameterEncodingFailed(reason: .jsonEncodingFailed(error: error))
+            var queryItems: [URLQueryItem] = []
+            for (key, value) in parameters {
+                queryItems.append(URLQueryItem(name: key, value: "\(value)"))
             }
+            urlComponents?.queryItems = queryItems
         }
+        
+        guard let finalURL = urlComponents?.url else {
+            throw AFError.invalidURL(url: "")
+        }
+        
+        var urlRequest = URLRequest(url: finalURL)
+        urlRequest.httpMethod = method.rawValue
         
         return urlRequest
     }
