38976c3f3822b5cff81740a71a207fef79cc75b5
kanat <kabsadyk@eub.kz>
2023-08-06 13:25:13 +0600

    add combine to router

--- GlobalWeather/Sources/Flows/FirstFlow/FlowCoordinator/Modules/MainModule/Router/MainRouter.swift
+++ GlobalWeather/Sources/Flows/FirstFlow/FlowCoordinator/Modules/MainModule/Router/MainRouter.swift
@@ -6,9 +6,20 @@
 //  
 //
 
-import Foundation
+import Combine
 
 class MainRouter: BaseModuleRouter<MainRoutingHandlingProtocol> {
+    private var cancellables = Set<AnyCancellable>()
+
+    override init(viewController: ViewControllerProtocol?) {
+        super.init(viewController: viewController)
+        
+    }
+    
+    deinit {
+        cancellables.forEach { $0.cancel() }
+        cancellables.removeAll()
+    }
 }
 
 extension MainRouter: MainRouterInputProtocol {
