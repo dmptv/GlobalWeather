ec95d47500ace8fcb870bb32e331c2ee0fad652d
kanat <kabsadyk@eub.kz>
2023-08-06 15:26:46 +0600

    separate asRequest into separate functions

--- GlobalWeather/Common/API/APIClientService/APIRouter.swift
+++ GlobalWeather/Common/API/APIClientService/APIRouter.swift
@@ -43,8 +43,21 @@ enum APIRouter {
         case .wheatherBy:
             return ""
         }
+    } 
+}
+
+extension APIRouter: APIRouteParameters {
+    var parameters: Parameters? {
+        var combined = commonParameters
+        switch self {
+        case let .city(cityName):
+            combined["q"] = cityName
+        case let .wheatherBy(location):
+            combined["lat"] = location.latitude
+            combined["lon"] = location.longitude
+        }
+        return combined
     }
-    
 }
 
 extension APIRouter: URLRequestConvertible {
@@ -53,40 +66,45 @@ extension APIRouter: URLRequestConvertible {
         var urlRequest = URLRequest(url: url.appendingPathComponent(path))
         urlRequest.httpMethod = method.rawValue
         
-        if let parameters = parameters {
-            if method == .get {
-                var urlComponents = URLComponents(url: url.appendingPathComponent(path), resolvingAgainstBaseURL: true)
-                var queryItems: [URLQueryItem] = []
-                for (key, value) in parameters {
-                    queryItems.append(URLQueryItem(name: key, value: "\(value)"))
-                }
-                urlComponents?.queryItems = queryItems
-                urlRequest.url = urlComponents?.url
-            } else if method == .post {
-                do {
-                    urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
-                } catch {
-                    throw AFError.parameterEncodingFailed(reason: .jsonEncodingFailed(error: error))
-                }
-            }
-        }
+        try configureParameters(parameters, method: method, urlRequest: &urlRequest)
         
         return urlRequest
     }
-}
-
-extension APIRouter: APIRouteParameters {
-    var parameters: Parameters? {
-        var combined = commonParameters
-        
-        switch self {
-        case let .city(cityName):
-            combined["q"] = cityName
-        case let .wheatherBy(location):
-            combined["lat"] = location.latitude
-            combined["lon"] = location.longitude
+    
+    private func configureParameters(_ parameters: Parameters?, method: HTTPMethod, urlRequest: inout URLRequest) throws {
+        guard let parameters = parameters else {
+            return
         }
         
-        return combined
+        switch method {
+        case .get:
+            try configureGetParameters(parameters, urlRequest: &urlRequest)
+        case .post:
+            try configurePostParameters(parameters, urlRequest: &urlRequest)
+        default:
+            break
+        }
+    }
+    
+    private func configureGetParameters(_ parameters: Parameters, urlRequest: inout URLRequest) throws {
+        var urlComponents = URLComponents(url: urlRequest.url!, resolvingAgainstBaseURL: true)
+        var queryItems: [URLQueryItem] = []
+        
+        for (key, value) in parameters {
+            queryItems.append(URLQueryItem(name: key, value: "\(value)"))
+        }
+        
+        urlComponents?.queryItems = queryItems
+        urlRequest.url = urlComponents?.url
+    }
+    
+    private func configurePostParameters(_ parameters: Parameters, urlRequest: inout URLRequest) throws {
+        do {
+            urlRequest.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
+        } catch {
+            throw AFError.parameterEncodingFailed(reason: .jsonEncodingFailed(error: error))
+        }
     }
 }
+
+
