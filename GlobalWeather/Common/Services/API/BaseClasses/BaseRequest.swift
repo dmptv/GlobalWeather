18a3ead25b22e23f01dfdc381795c154b62e0163
kanat <kabsadyk@kz.kz>
2023-08-17 08:39:04 +0600

    remove prints

--- GlobalWeather/Common/Services/API/BaseClasses/BaseRequest.swift
+++ GlobalWeather/Common/Services/API/BaseClasses/BaseRequest.swift
@@ -28,18 +28,6 @@ class BaseRequest<R: RouterProtocol, T: Decodable> {
 
                     switch response.result {
                     case .success(let value):
-                        
-                        
-                        if let locationWeatherModel = value as? LocationWeatherModel {
-                            print("Response:")
-                            dump(locationWeatherModel)
-                            
-                            if let data = response.data,
-                               let jsonString = String(data: data, encoding: .utf8) {
-                                print("Incoming JSON response:\n", jsonString)
-                            }
-                        }
-                        
                         promise(.success(value))
                     case .failure(let afError):
                         promise(.failure(CustomAPIError.requestFailed(description: afError.localizedDescription)))
