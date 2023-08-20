ae1f648dc3bb072eefaa973624c14236feff71f3
kanat <kanat@com.com>
2023-08-20 19:52:47 +0600

    cell for item

--- GlobalWeather/Sources/Flows/FirstFlow/FlowCoordinator/Modules/MultyCompositeModule/Submodules/HourForecastModule/View/HourForecastViewController.swift
+++ GlobalWeather/Sources/Flows/FirstFlow/FlowCoordinator/Modules/MultyCompositeModule/Submodules/HourForecastModule/View/HourForecastViewController.swift
@@ -27,6 +27,30 @@ extension HourForecastViewController: HourForecastViewInput {
     
     private func setupCollectionView() {
         hourCollectionView.backgroundColor = .emerald
+        
+        
+        hourCollectionView.register(cellType: UICollectionViewCell.self)
+        hourCollectionView.dataSource = self
+    }
+}
+
+extension HourForecastViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(for: indexPath, cellType: UICollectionViewCell.self)
+        
+        cell.contentView.backgroundColor = .red
+        
+        return cell
+    }
+}
+
+extension HourForecastViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.bounds.height, height: view.bounds.height)
     }
 }
 
